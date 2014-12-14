3d647800e5719c3edc9f5d6597ab2c34802974b3
satanas <wil.alejandro@gmail.com>
2014-12-14 11:56:50 -0300

    Separated all the player stuff into hero.js module

--- game.js
+++ game.js
@@ -29,28 +29,11 @@ Game.prototype = {
     //layer.debug = true;
     this.layer.resizeWorld();
 
-    //player = new Hero(250, 170);
-    this.player = this.game.add.sprite(250, 170, 'alysa');
-    this.player.jumping = false;
-    this.player.facing = 'right';
-    this.player.canShoot = true;
-    this.player.canDoubleJump = false;
-    this.player.doubleJumping = false;
-    this.player.lastShotTime = 0;
-    this.player.shooting = false;
-    this.player.death = false;
-
-    this.player.animations.add('right', [0, 1, 2, 3, 4, 5, 6, 7], 12, true);
-    this.player.animations.add('left',  [8, 9, 10, 11, 12, 13, 14, 15], 12, true);
-
-    this.game.physics.arcade.enable(this.player);
-    this.player.body.gravity.y = 1000;
-    this.player.body.maxVelocity.y = 500;
-    this.player.body.maxVelocity.x = 200;
-    this.player.body.setSize(22, 45, 8, 7);
-    this.game.camera.follow(this.player);
-
     this.cursors = this.game.input.keyboard.createCursorKeys();
+
+    this.player = new Alysa(this.game, this.cursors, 250, 170);
+    this.game.add.existing(this.player);
+
     this.game.world.bringToTop(this.bullets);
 
     this.gumbon = new Gumbon(this.game, 100, 100, 0);
@@ -68,104 +51,6 @@ Game.prototype = {
     this.game.physics.arcade.collide(this.player, this.layer);
     this.game.physics.arcade.collide(this.gumbon, this.layer);
     this.game.physics.arcade.collide(this.bullets, this.layer, this.bulletCollision);
-
-    if (this.player.body.onFloor()) {
-      this.player.jumping = false;
-      this.player.doubleJumping = false;
-      this.player.canDoubleJump = false;
-    }
-
-    if (this.cursors.left.isDown) {
-      this.player.facing = 'left';
-      this.player.body.velocity.x = -200;
-    } else if (this.cursors.right.isDown) {
-      this.player.facing = 'right';
-      this.player.body.velocity.x = 200;
-    } else {
-      this.player.body.velocity.x = 0;
-    }
-
-    //if (this.cursors.up.isDown && !this.player.jumping) {
-    if (this.cursors.up.justPressed(50) && !this.player.jumping) {
-      this.player.jumping = true;
-      this.player.body.velocity.y = -800;
-    }
-    // Free fall
-    if (this.player.body.velocity.y !== 0 && !this.player.jumping) {
-      this.player.jumping = true;
-      this.player.canDoubleJump = true;
-    }
-
-    if (this.cursors.up.justReleased(50) && this.player.jumping && !this.player.doubleJumping) {
-      this.player.canDoubleJump = true;
-    }
-
-    if (this.cursors.up.isDown && this.player.jumping && this.player.canDoubleJump) {
-      this.player.doubleJumping = true;
-      this.player.canDoubleJump = false;
-      this.player.body.velocity.y = -500;
-    }
-
-    if (this.game.input.keyboard.justPressed(Phaser.Keyboard.X) && !this.player.shooting && this.player.canShoot) {
-      this.player.shooting = true;
-      var bullet = this.bullets.create(this.player.body.x + (this.player.body.width / 2), this.player.body.y + (this.player.body.height / 2), 'bullet');
-      bullet.body.allowGravity = false;
-      bullet.bringToTop();
-      if (this.player.facing === 'left') {
-        bullet.body.velocity.x = -1 * this.game.global.maxBulletSpeed;
-      } else {
-        bullet.body.velocity.x = this.game.global.maxBulletSpeed;
-      }
-    }
-
-    if (this.game.input.keyboard.justReleased(Phaser.Keyboard.X)) {
-      this.player.shooting = false;
-    }
-
-    // Render
-    if (this.player.jumping === true) {
-      if (this.player.facing == 'left') {
-        this.player.frame = 19;
-      } else {
-        this.player.frame = 18;
-      }
-    } else if (this.player.body.velocity.x !== 0 && this.player.body.velocity.y === 0) {
-      if (this.player.facing == 'left') {
-        this.player.animations.play('left');
-      } else if (this.player.facing == 'right') {
-        this.player.animations.play('right');
-      }
-    } else {
-      this.player.animations.stop();
-      if (this.player.facing == 'left') {
-        this.player.frame = 17;
-      } else {
-        this.player.frame = 16;
-      }
-    }
-
-    // Collide with bounds. We only detect the side bounds
-    if (this.player.x >= this.game.world.width - this.player.width) {
-      this.player.x = this.game.world.width - this.player.width;
-    }
-    if (this.player.x <= 0) {
-      this.player.x = 0;
-    }
-
-
-    // Detect death by falling
-    if (this.player.y > this.game.world.height && !this.player.death) {
-      var self = this;
-      this.player.death = true;
-      this.game.plugin.fadeOut(0x000, 750, 0, function() {
-        self.game.state.start('gameover');
-      });
-    }
-
-    //if (this.player.x > 500) {
-    //  this.player.death = true;
-    //  this.game.plugin.fadeOut(0x000, 2000);
-    //}
   },
 
   render: function() {
---
+++ hero.js
@@ -0,0 +1,123 @@
+'use strict';
+
+var Alysa = function(game, cursors, x, y) {
+  Phaser.Sprite.call(this, game, x, y, 'alysa', 0);
+
+  this.cursors = cursors;
+  this.jumping = false;
+  this.facing = 'right';
+  this.canShoot = true;
+  this.canDoubleJump = false;
+  this.doubleJumping = false;
+  this.lastShotTime = 0;
+  this.shooting = false;
+  this.death = false;
+
+  this.animations.add('right', [0, 1, 2, 3, 4, 5, 6, 7], 12, true);
+  this.animations.add('left',  [8, 9, 10, 11, 12, 13, 14, 15], 12, true);
+
+  this.game.physics.arcade.enable(this);
+  this.body.gravity.y = 1000;
+  this.body.maxVelocity.y = 500;
+  this.body.maxVelocity.x = 200;
+  this.body.setSize(22, 45, 8, 7);
+  this.game.camera.follow(this);
+};
+
+Alysa.prototype = Object.create(Phaser.Sprite.prototype);
+Alysa.prototype.constructor = Alysa;
+
+Alysa.prototype.update = function() {
+  if (this.body.onFloor()) {
+    this.jumping = false;
+    this.doubleJumping = false;
+    this.canDoubleJump = false;
+  }
+
+  if (this.cursors.left.isDown) {
+    this.facing = 'left';
+    this.body.velocity.x = -200;
+  } else if (this.cursors.right.isDown) {
+    this.facing = 'right';
+    this.body.velocity.x = 200;
+  } else {
+    this.body.velocity.x = 0;
+  }
+
+  //if (this.cursors.up.isDown && !this.jumping) {
+  if (this.cursors.up.justPressed(50) && !this.jumping) {
+    this.jumping = true;
+    this.body.velocity.y = -800;
+  }
+  // Free fall
+  if (this.body.velocity.y !== 0 && !this.jumping) {
+    this.jumping = true;
+    this.canDoubleJump = true;
+  }
+
+  if (this.cursors.up.justReleased(50) && this.jumping && !this.doubleJumping) {
+    this.canDoubleJump = true;
+  }
+
+  if (this.cursors.up.isDown && this.jumping && this.canDoubleJump) {
+    this.doubleJumping = true;
+    this.canDoubleJump = false;
+    this.body.velocity.y = -500;
+  }
+
+  if (this.game.input.keyboard.justPressed(Phaser.Keyboard.X) && !this.shooting && this.canShoot) {
+    this.shooting = true;
+    var bullet = this.bullets.create(this.body.x + (this.body.width / 2), this.body.y + (this.body.height / 2), 'bullet');
+    bullet.body.allowGravity = false;
+    bullet.bringToTop();
+    if (this.facing === 'left') {
+      bullet.body.velocity.x = -1 * this.game.global.maxBulletSpeed;
+    } else {
+      bullet.body.velocity.x = this.game.global.maxBulletSpeed;
+    }
+  }
+
+  if (this.game.input.keyboard.justReleased(Phaser.Keyboard.X)) {
+    this.shooting = false;
+  }
+
+  // Render
+  if (this.jumping === true) {
+    if (this.facing == 'left') {
+      this.frame = 19;
+    } else {
+      this.frame = 18;
+    }
+  } else if (this.body.velocity.x !== 0 && this.body.velocity.y === 0) {
+    if (this.facing == 'left') {
+      this.animations.play('left');
+    } else if (this.facing == 'right') {
+      this.animations.play('right');
+    }
+  } else {
+    this.animations.stop();
+    if (this.facing == 'left') {
+      this.frame = 17;
+    } else {
+      this.frame = 16;
+    }
+  }
+
+  // Collide with bounds. We only detect the side bounds
+  if (this.x >= this.game.world.width - this.width) {
+    this.x = this.game.world.width - this.width;
+  }
+  if (this.x <= 0) {
+    this.x = 0;
+  }
+
+
+  // Detect death by falling
+  if (this.y > this.game.world.height && !this.death) {
+    var self = this;
+    this.death = true;
+    this.game.plugin.fadeOut(0x000, 750, 0, function() {
+      self.game.state.start('gameover');
+    });
+  }
+};
