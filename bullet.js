9d8f781d9e477670f86fa6c808d6affe8ba699e8
satanas <wil.alejandro@gmail.com>
2014-12-26 19:48:46 -0300

    Destroying bullets after collision. Added enemy bullets

--- bullet.js
+++ bullet.js
@@ -22,13 +22,47 @@ Bullet.prototype.constructor = Bullet;
 Bullet.prototype.update = function() {
   this.game.physics.arcade.collide(this, groups.platforms, this.die);
   this.game.physics.arcade.overlap(this, groups.enemies, this.makeDamage);
+  if (!this.alive) {
+    this.destroy();
+  }
 };
 
 Bullet.prototype.die = function(self, platform) {
-  self.destroy();
+  self.kill();
 };
 
 Bullet.prototype.makeDamage = function(self, object) {
   object.takeDamage();
-  self.destroy();
+  self.kill();
+};
+
+var EnemyBullet = function(game, x, y, direction) {
+  Phaser.Sprite.call(this, game, x, y, 'bullet', 0);
+
+  this.type = 'bullet';
+  this.speed = 600;
+  this.game.physics.arcade.enable(this);
+  this.body.allowGravity = false;
+  this.body.sensor = true;
+  this.bringToTop();
+  if (direction === 'left') {
+    this.body.velocity.x = -1 * this.speed;
+  } else {
+    this.body.velocity.x = this.speed;
+  }
+  groups.enemies.add(this);
+};
+
+EnemyBullet.prototype = Object.create(Phaser.Sprite.prototype);
+EnemyBullet.prototype.constructor = EnemyBullet;
+
+EnemyBullet.prototype.update = function() {
+  this.game.physics.arcade.collide(this, groups.platforms, this.die);
+  if (!this.alive) {
+    this.destroy();
+  }
+};
+
+EnemyBullet.prototype.die = function(self, platform) {
+  self.kill();
 };
