a1fc7b15e543e41bdca1f5a6a8728d7fb2578cba
satanas <wil.alejandro@gmail.com>
2014-12-25 22:53:54 -0300

    Added trap mechanics

---
+++ traps.js
@@ -0,0 +1,12 @@
+'use strict';
+
+var Spike = function(game, x, y) {
+  Phaser.Sprite.call(this, game, x, y, 'spikes', 0);
+
+  this.game.physics.arcade.enable(this);
+  this.body.allowGravity = false;
+  groups.enemies.add(this);
+};
+
+Spike.prototype = Object.create(Phaser.Sprite.prototype);
+Spike.prototype.constructor = Spike;
