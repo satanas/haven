c38682415e5b19a7f51e96f48e289aa379de92c4
satanas <wil.alejandro@gmail.com>
2014-12-27 11:48:33 -0300

    Fixed enemy bullet collisions

--- bullet.js
+++ bullet.js
@@ -5,6 +5,8 @@ var Bullet = function(game, x, y, direction) {
 
   this.speed = 600;
   this.game.physics.arcade.enable(this);
+  this.checkWorldBounds = true;
+  this.outOfBoundsKill = true;
   this.body.allowGravity = false;
   this.body.sensor = true;
   this.bringToTop();
@@ -39,10 +41,13 @@ Bullet.prototype.makeDamage = function(self, object) {
 var EnemyBullet = function(game, x, y, direction) {
   Phaser.Sprite.call(this, game, x, y, 'bullet', 0);
 
-  this.type = 'bullet';
+  //this.type = 'bullet';
+  this.tint = 0xcd0937;
   this.speed = 600;
   this.game.physics.arcade.enable(this);
   this.body.allowGravity = false;
+  this.checkWorldBounds = true;
+  this.outOfBoundsKill = true;
   this.body.sensor = true;
   this.bringToTop();
   if (direction === 'left') {
@@ -50,14 +55,14 @@ var EnemyBullet = function(game, x, y, direction) {
   } else {
     this.body.velocity.x = this.speed;
   }
-  groups.enemies.add(this);
+  groups.bullets.add(this);
 };
 
 EnemyBullet.prototype = Object.create(Phaser.Sprite.prototype);
 EnemyBullet.prototype.constructor = EnemyBullet;
 
 EnemyBullet.prototype.update = function() {
-  this.game.physics.arcade.collide(this, groups.platforms, this.die);
+  this.game.physics.arcade.overlap(this, groups.platforms, this.die);
   if (!this.alive) {
     this.destroy();
   }
