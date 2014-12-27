c92cd8d69e9d3956d7b38c3d94ab627781a35fb6
satanas <wil.alejandro@gmail.com>
2014-12-27 12:31:13 -0300

    Added some improvements to the blood particles

--- particles.js
+++ particles.js
@@ -4,6 +4,9 @@ var BloodParticles = function(game, x, y) {
   var emitter = game.add.emitter(x, y, 15);
   emitter.makeParticles('blood');
   emitter.gravity = 500;
+  emitter.minParticleSpeed.setTo(-100, -200);
+  emitter.maxParticleSpeed.setTo(50, 100);
+  emitter.angularDrag = 10;
   emitter.start(true, 2000, null, 5);
 };
 
