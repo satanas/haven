8faa29c1d01fc011e3c3472e0ea67f32f768016a
satanas <wil.alejandro@gmail.com>
2015-01-27 00:51:35 -0300

    Cleaned up clock code

--- src/clock.js
+++ src/clock.js
@@ -11,7 +11,6 @@ var Clock = function(game) {
 Clock.prototype.start = function(time) {
   this.maxTime = time * 1000;
   this.time = time * 1000;
-  //this.looping = game.time.events.loop(1000, this.onLoop, this);
 };
 
 Clock.prototype.update = function() {
@@ -26,13 +25,6 @@ Clock.prototype.update = function() {
 Clock.prototype.getTime = function() {
   return parseInt(this.time / 1000);
 };
-
-//Clock.prototype.onLoop = function() {
-//  this.time -= 1;
-//  if (this.time <= 0) {
-//    this.ended = true;
-//  }
-//};
 
 Clock.prototype.reset = function() {
   this.time = this.maxTime;
