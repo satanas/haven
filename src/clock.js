0f623afc1296a9b9638b5b7165fa16a0210b12ce
satanas <wil.alejandro@gmail.com>
2015-01-27 00:45:17 -0300

    Improved clock

--- src/clock.js
+++ src/clock.js
@@ -1,26 +1,41 @@
 'use strict';
 
-var Clock = function(time, game) {
-  this.maxTime = time;
-  this.time = time;
+var Clock = function(game) {
+  this.maxTime = 0;
+  this.time = 0;
+  this.game = game;
   this.ended = false;
   this.looping = null;
 };
 
-Clock.prototype.start = function() {
-  this.looping = game.time.events.loop(1000, this.onLoop, this);
+Clock.prototype.start = function(time) {
+  this.maxTime = time * 1000;
+  this.time = time * 1000;
+  //this.looping = game.time.events.loop(1000, this.onLoop, this);
 };
 
-Clock.prototype.onLoop = function() {
-  this.time -= 1;
-  if (this.time <= 0) {
-    this.ended = true;
+Clock.prototype.update = function() {
+  if (!this.ended) {
+    this.time -= this.game.time.elapsed;
+    if (this.time <= 0) {
+      this.ended = true;
+    }
   }
 };
+
+Clock.prototype.getTime = function() {
+  return parseInt(this.time / 1000);
+};
+
+//Clock.prototype.onLoop = function() {
+//  this.time -= 1;
+//  if (this.time <= 0) {
+//    this.ended = true;
+//  }
+//};
 
 Clock.prototype.reset = function() {
   this.time = this.maxTime;
   this.ended = false;
-  this.looping = null;
-  this.start();
+  //this.looping = null;
 };
