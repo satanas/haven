de562b11fea35a607597da6351c8a714fae74010
satanas <wil.alejandro@gmail.com>
2015-01-31 12:21:28 -0300

    Added BGM for the intro

--- src/menu.js
+++ src/menu.js
@@ -10,6 +10,12 @@ Menu.prototype = {
     this.game.stage.backgroundColor = '#000';
     this.currentOption = 'start';
     this.background = this.game.add.sprite(0, 0, 'menu');
+    this.bgmSound = this.game.add.audio('bgmintro', 1, true);
+    this.bgmSound.onDecoded.add(this.start, this);
+  },
+
+  start: function() {
+    this.bgmSound.play();
   },
 
   update: function() {
