31ef0e919b86a01417f1ae3e10bb726c191927d6
satanas <wil.alejandro@gmail.com>
2015-04-19 12:37:54 -0300

    Added param to fix the centered label to the camera

--- src/text.js
+++ src/text.js
@@ -1,8 +1,12 @@
 'use strict';
 
-function bitmapTextCentered(y, font, text, size) {
+function bitmapTextCentered(y, font, text, size, cameraOffset) {
   var label = game.add.bitmapText(0, y, font, text, size);
   label.updateTransform();
   label.x = (game.width - label.width) / 2;
+  if (cameraOffset) {
+    label.y += game.camera.y;
+    label.x += game.camera.x;
+  }
   return label;
 }
