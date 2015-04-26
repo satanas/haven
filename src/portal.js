30be3d992ecb6e5fef39e38f4827234700a940fc
satanas <wil.alejandro@gmail.com>
2015-04-25 23:10:56 -0400

    Fixed animation and added body size to portals

--- src/portal.js
+++ src/portal.js
@@ -4,10 +4,11 @@ var Portal = function(x, y) {
   Phaser.Sprite.call(this, game, x, y, 'portal', 0);
 
   game.physics.arcade.enable(this);
+  this.body.setSize(27, 96, 74, 96);
   this.body.allowGravity = false;
   this.body.sensor = true;
 
-  this.animations.add('main', null, 20, true);
+  this.animations.add('main', null, 30, true);
   this.animations.play('main');
   groups.portals.add(this);
 };
