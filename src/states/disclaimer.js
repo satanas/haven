cf6fc7780bee057c920c0c0a6b55d9ea2f5584fd
satanas <wil.alejandro@gmail.com>
2015-05-23 23:43:38 -0400

    Fixed some text in disclaimer

--- src/states/disclaimer.js
+++ src/states/disclaimer.js
@@ -6,7 +6,7 @@ var Disclaimer = {
     bitmapTextCentered(60, 'press_start', 'Demo disclaimer', 30);
     bitmapTextCentered(150, 'press_start', 'This release has the sole purpose of', 16);
     bitmapTextCentered(180, 'press_start', 'demonstrate the basis of the game', 16);
-    bitmapTextCentered(210, 'press_start', 'mechanic. Esential parts of the story', 16);
+    bitmapTextCentered(210, 'press_start', 'mechanic. Some parts of the story', 16);
     bitmapTextCentered(240, 'press_start', 'were removed to avoid spoilers', 16);
     bitmapTextCentered(440, 'press_start', 'Press Enter to continue', 12);
 
